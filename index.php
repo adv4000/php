9d7636e45741a7c0f76c59d4ab49a6edd6d057b9
Denis Astahov <adv4000@gmail.com>
2020-07-07 09:37:34 -0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,19 @@
+<html>
+<head>
+<style>body { background-color: white }</style>
+</head>
+<body>
+<center>
+<font color="green"><h1>Hello from Denis Astahov!</h1></font>
+<img src="logo.png" width=400>
+
+<?php
+echo '<p style="color: gold">
+      This is AWS Elastic Beanstalk WebServer with PHP App!!!!!!!!!!!!!!</p>';
+echo '<br>Server IP:'.$_SERVER['SERVER_ADDR'];
+?>
+<br>
+Version 2.0
+</center>
+</body>
+</html>
